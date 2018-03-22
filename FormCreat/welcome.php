f7fc89134c82f55a4c7cc19054de71126a39941d
zhangxianqiang <zhangxianqiangmacbook@zhangxianqiang.local>
2018-03-22 17:07:19 +0800

    尼玛啊还需要配置才能搞起来啊

---
+++ FormCreat/welcome.php
@@ -0,0 +1,10 @@
+欢迎<?php echo $_POST["fname"]; ?>!<br>
+你的年龄是 <?php echo $_POST["age"]; ?>  岁。
+<?php
+//
+//print_r($_POST);
+//?>
+<!--<form action="welcome.php" method="post">-->
+<!--    <input type="text" name="age">-->
+<!--    <input type="submit" value="test">-->
+<!--</form>-->
